908fa952108832cd52eb54d73740c3c25b6262ca
rifathossain290 <rifathossain2252@gmail.com>
2023-03-18 03:29:45 -0700

    responsive done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,142 @@
+'use strict';
+
+/* ------------------------------- mobile menu start ------------------------------ */
+let show = document.querySelector('.show');
+let menuClose = document.querySelector('.menu-close');
+let menuItem = document.querySelector('.menu-item');
+
+function showMenu() {
+    menuItem.style.opacity = "1";
+    menuItem.style.visibility = "visible";
+    menuItem.style.left = "0";
+    menuItem.style.transition = "all .3s ease 0s";
+    show.style.display = "none";
+    menuClose.style.display = "block";
+}
+function hideMenu() {
+    menuItem.style.opacity = "0";
+    menuItem.style.visibility = "hidden";
+    menuItem.style.left = "100%";
+    menuItem.style.transition = "all .3s ease 0s";
+    show.style.display = "block";
+    menuClose.style.display = "none";
+}
+show.addEventListener('click', showMenu);
+menuClose.addEventListener('click', hideMenu);
+
+
+/* ------------------------------- mobile menu end ------------------------------ */
+
+
+/* ---------------------------- footer menu start --------------------------- */
+let up = document.querySelector('.up');
+let down = document.querySelector('.down');
+let footerNav = document.querySelector('.footer-nav');
+
+
+function showFooter() {
+    footerNav.style.display = 'block';
+    up.style.display = 'block';
+    down.style.display = 'none';
+    
+}
+
+function hideFooter(){
+    footerNav.style.display = 'none';
+    up.style.display = 'none';
+    down.style.display = 'block';
+}
+
+down.addEventListener('click', showFooter);
+up.addEventListener('click', hideFooter);
+
+
+
+
+/* ---------------------------- footer menu end --------------------------- */
+
+
+
+
+
+
+
+
+/* progress chart functionality
+------------------------------------- */
+
+let number1 = document.querySelector('.number-1');
+let number2 = document.querySelector('.number-2');
+let number3 = document.querySelector('.number-3');
+let number4 = document.querySelector('.number-4');
+let counter1 = 0;
+let counter2 = 0;
+let counter3 = 0;
+let counter4 = 0;
+
+
+// counter1
+setInterval(() => {
+    if(counter1 == 90) {
+        clearInterval();
+    } else {
+        counter1 += 1;
+        number1.innerHTML = counter1;
+    }
+}, 23);
+// counter2
+setInterval(() => {
+    if(counter2 == 75) {
+        clearInterval();
+    } else {
+        counter2 += 1;
+        number2.innerHTML = counter2;
+    }
+}, 22);
+// counter3
+setInterval(() => {
+    if(counter3 == 70) {
+        clearInterval();
+    } else {
+        counter3 += 1;
+        number3.innerHTML = counter3;
+    }
+}, 23);
+// counter4
+setInterval(() => {
+    if(counter4 == 85) {
+        clearInterval();
+    } else {
+        counter4 += 1;
+        number4.innerHTML = counter4;
+    }
+}, 30);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                              jquery Activation                             */
+/* -------------------------------------------------------------------------- */
+
 $(document).ready(function(){
     // slider active
     $('.slider-active').owlCarousel({
@@ -9,8 +148,8 @@ $(document).ready(function(){
             0:{
                 items:1
             },
-            600:{
-                items:3
+            768:{
+                items:1
             },
             992:{
                 items:1
@@ -52,55 +191,6 @@ $(document).ready(function(){
 })
 
 
-/* progress chart functionality
-------------------------------------- */
-
-let number1 = document.querySelector('.number-1');
-let number2 = document.querySelector('.number-2');
-let number3 = document.querySelector('.number-3');
-let number4 = document.querySelector('.number-4');
-let counter1 = 0;
-let counter2 = 0;
-let counter3 = 0;
-let counter4 = 0;
-
-
-// counter1
-setInterval(() => {
-    if(counter1 == 90) {
-        clearInterval();
-    } else {
-        counter1 += 1;
-        number1.innerHTML = counter1;
-    }
-}, 20);
-// counter2
-setInterval(() => {
-    if(counter2 == 75) {
-        clearInterval();
-    } else {
-        counter2 += 1;
-        number2.innerHTML = counter2;
-    }
-}, 27);
-// counter3
-setInterval(() => {
-    if(counter3 == 70) {
-        clearInterval();
-    } else {
-        counter3 += 1;
-        number3.innerHTML = counter3;
-    }
-}, 25);
-// counter4
-setInterval(() => {
-    if(counter4 == 85) {
-        clearInterval();
-    } else {
-        counter4 += 1;
-        number4.innerHTML = counter4;
-    }
-}, 21);
 
 
 
